72b48fb364a3573cea8c70b7188955e9c88e70e3
Elbert Bae <elbert.bae@gmail.com>
2020-04-19 14:47:00 -0700

    Add basic background and positioning of crawl

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
-
+import './styles/App.css';
 // Components
 import StarwarsBackground from './components/starwars-crawl/starwars-background'
 
